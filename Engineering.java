8526d56b85f898a34f22af18f862e546261aa925
Henry Elliott <henrylovesscience@gmail.com>
2015-08-01 18:04:48 -0500

    Make working version

--- Engineering.java
+++ Engineering.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Engineering {
@@ -21,7 +22,14 @@ public class Engineering {
 			return;
 		}
 
-		interactiveLoop(records);
+		try {
+			interactiveLoop(records);
+		} catch (IOException ex) {
+			System.err.println("Unexpected IOException:");
+			ex.printStackTrace();
+			System.exit(1);
+			return;
+		}
 }
 
 	private static ArrayList<Student> readRecords(File file)
@@ -38,12 +46,55 @@ public class Engineering {
 		return records;
 	}
 	
-	private static void interactiveLoop(ArrayList<Student> records) {
-		BufferedReader stdin = new BufferedReader(new FileInputStream(
-		                       System.in));
-		for (String line; (line = stdin.readLine()) != null;) {
-			System.out.println(line);
+	private static void interactiveLoop(ArrayList<Student> records)
+	        throws IOException {
+		try (BufferedReader stdin = new BufferedReader(new InputStreamReader(
+			                        System.in))) {
+			for (String line; (line = stdin.readLine()) != null;) {
+				String[] tokens = line.split("\\s+");
+				try {
+					String[] a = tokens[0].split("=");
+					String[] b = tokens[1].split("=");
+					String[] c = tokens[2].split("=");
+					ArrayList<Student> satisfyA = new ArrayList<Student>();
+					for (Student record : records) {
+						if (record.satisfies(a)) {
+							satisfyA.add(record);
+						}
+					}
+					int both = 0;
+					int first = 0;
+					int second = 0;
+					int neither = 0;
+					for (Student record : satisfyA) {
+						boolean satisfiesB = record.satisfies(b);
+						boolean satisfiesC = record.satisfies(c);
+						if (satisfiesB && satisfiesC) {
+							both++;
+						} else if (satisfiesB) {
+							first++;
+						} else if (satisfiesC) {
+							second++;
+						} else {
+							neither++;
+						}
+					}
+					int total = satisfyA.size();
+					System.out.printf("%d / %d satisfy both.\n", both, total);
+					System.out.printf("%d / %d satisfy only the first.\n",
+					                  first, total);
+					System.out.printf("%d / %d satisfy only the second.\n",
+					                  second, total);
+					System.out.printf("%d / %d satisfy neither.\n",
+					                  neither, total);
+				} catch (ArrayIndexOutOfBoundsException ex) {
+					System.out.println("Error parsing input.");
+					continue;
+				} catch (RuntimeException ex) {
+					System.out.println(ex.getMessage());
+					continue;
+				}
+			}
 		}
-
 	}
 }
--- Student.java
+++ Student.java
@@ -1,25 +1,29 @@
 public class Student {
 	/* Public data, because this class is immutable, this data can be public */
-	public final String last;
-	public final String first;
-	public final boolean male;
-	public final String ethnicity;
-	public final boolean pltw;
-	public final boolean ap;
-	public final boolean upbound;
-	public final boolean lego;
-	public final boolean best;
-	public final boolean camp;
-	public final boolean seeds;
-	public final boolean family;
-	public final boolean community;
-	public final boolean visit;
+	private final String last;
+	private final String first;
+	private final String gender;
+	private final String ethnicity;
+	private final boolean pltw;
+	private final boolean ap;
+	private final boolean upbound;
+	private final boolean lego;
+	private final boolean best;
+	private final boolean camp;
+	private final boolean seeds;
+	private final boolean family;
+	private final boolean community;
+	private final boolean visit;
+
+	public static final String[] names = {"last", "first", "gender", "ethnicity",
+	        "pltw", "ap", "upbound", "lego", "best", "camp", "seeds", "family",
+	        "community", "visit" };
 
 	public Student(String line) {
-		String[] fields = line.split(",");
+		String[] fields = line.split(",", -1);
 		last = fields[0];
 		first = fields[1];
-		male = !fields[2].isEmpty();
+		gender = fields[2];
 		ethnicity = fields[3];
 		pltw = !fields[4].isEmpty();
 		ap = !fields[5].isEmpty();
@@ -33,4 +37,39 @@ public class Student {
 		visit = !fields[13].isEmpty();
 	}
 
+	public boolean satisfies(String[] varVal) {
+		String var = varVal[0];
+		String val = varVal[1];
+		boolean valBool = val.equalsIgnoreCase("true");
+		if (var.equalsIgnoreCase(names[0])) {
+			return val.equalsIgnoreCase(last);
+		} else if (var.equalsIgnoreCase(names[1])) {
+			return val.equalsIgnoreCase(first);
+		} else if (var.equalsIgnoreCase(names[2])) {
+			return val.equalsIgnoreCase(gender);
+		} else if (var.equalsIgnoreCase(names[3])) {
+			return val.equalsIgnoreCase(ethnicity);
+		} else if (var.equalsIgnoreCase(names[4])) {
+			return valBool == pltw;
+		} else if (var.equalsIgnoreCase(names[5])) {
+			return valBool == ap;
+		} else if (var.equalsIgnoreCase(names[6])) {
+			return valBool == upbound;
+		} else if (var.equalsIgnoreCase(names[7])) {
+			return valBool == lego;
+		} else if (var.equalsIgnoreCase(names[8])) {
+			return valBool == best;
+		} else if (var.equalsIgnoreCase(names[9])) {
+			return valBool == camp;
+		} else if (var.equalsIgnoreCase(names[10])) {
+			return valBool == seeds;
+		} else if (var.equalsIgnoreCase(names[11])) {
+			return valBool == family;
+		} else if (var.equalsIgnoreCase(names[12])) {
+			return valBool == community;
+		} else if (var.equalsIgnoreCase(names[13])) {
+			return valBool == visit;
+		}
+		throw new RuntimeException("Unrecognized variable name: " + var);
+	}
 }
